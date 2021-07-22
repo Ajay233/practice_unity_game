2b6e8baf64febeacfd76732cdf9ae446cd2a5d1c
Ajay Mungur <ajaymungur@hotmail.com>
2021-07-23 00:32:17 +0100

    Animated Main menu, added animated tile and added easter eggs

--- New Unity Project/Assets/Scripts/MainMenu.cs
+++ New Unity Project/Assets/Scripts/MainMenu.cs
@@ -5,10 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private AudioClip _audioClip;
+    private AudioSource _audioSource;
+    private AudioSource _canvasAudio;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _audioSource = GameObject.Find("Audio Source").GetComponent<AudioSource>();
+        if (_audioSource == null)
+        {
+            Debug.LogError("Unable to find Audio Source");
+        }
+
+        _canvasAudio = GameObject.Find("Canvas").GetComponent<AudioSource>();
+        if (_canvasAudio == null)
+        {
+            Debug.LogError("Unable to find canvas Audio Source");
+        }
     }
 
     // Update is called once per frame
@@ -20,5 +33,19 @@ public class MainMenu : MonoBehaviour
     public void SinglePlayer()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void MultiPlayer()
+    {
+        _audioSource.clip = _audioClip;
+        _canvasAudio.Pause();
+        StartCoroutine(playAudio());
+    }
+
+    IEnumerator playAudio()
+    {
+        _audioSource.Play();
+        yield return new WaitForSeconds(3.2f);
+        _canvasAudio.Play();
     }
 }
