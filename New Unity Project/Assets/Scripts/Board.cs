d252b3dc539ba462e86d59698b38274a3f5d9ada
Aaron Gill <izzydiggler@gmail.com>
2021-07-24 22:23:17 +0100

    Added board prefab

---
+++ New Unity Project/Assets/Scripts/Board.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    private int width = 10;
+    private int height = 10;
+    private int[,] gridArr;
+    [SerializeField]
+    GameObject tile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gridArr = new int[width, height];
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Instantiate(tile, new Vector3(x, y, 0), Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
