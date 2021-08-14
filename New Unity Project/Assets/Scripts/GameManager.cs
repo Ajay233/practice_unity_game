0ae4f6ce303a6f30186a0560b76057d07db65293
Ajay Mungur <ajaymungur@hotmail.com>
2021-08-14 17:48:28 +0100

    This branch implements Krispin
    - Created 2 kraken prefabs, the only difference is the tag so we can
    treat them differently e.g. Only executing ReleaseTheKraken if the
    placeholder krispin is hit, which prevents the actual active krispin
    from being hit
    - Created a temporary skull prefab to represent where Krispin was hit
    - When the game starts a placeholder ksirpin is randomly placed on a
    unoccupied tile.  I reused the randomUnusedNumber generator method to
    make sure Krispin is only ever placed on a tile that unoccupied
    - When the krispin placeholder is hit, a new instance is created on the
    opposite board and the placeholder krispin is destroyed
    - On placement on the opposite board, krispin checks if there is a ship
    and destroys it if there is.
    - Implemented turn based system that is part executed in GameManager and part in the Krispin prefab's
    own script.
    - Prevented Krispin or the enemy from destroying a ship that's already
    been hit.
    - Prevented the player from clicking on a skull and wasting a go.  The
    enemy didn't need that kind of logic because the enemy only ever visits
    a tile once.
    - Updated the method for resetting the game variables

--- New Unity Project/Assets/Scripts/GameManager.cs
+++ New Unity Project/Assets/Scripts/GameManager.cs
@@ -11,6 +11,8 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject _EnemyBoard;
     [SerializeField] private GameObject _ship;
     [SerializeField] private GameObject _krispin;
+    [SerializeField] private GameObject _krispinPlaceholder;
+    [SerializeField] private GameObject _skull;
     [SerializeField] private int _shipLimit;
     [SerializeField] GameObject _missedPrefab;
     [SerializeField] GameObject _hitPrefab;
@@ -22,6 +24,11 @@ public class GameManager : MonoBehaviour
     private int _enemyShipCount = 0;
     bool _gameStart = false;
     private bool _playerTurn = false;
+    private bool _isKrakenTurn = false;
+    private GameObject _krispinOnPlayerBoard;
+    private GameObject _krispinOnEnemyBoard;
+    private bool _playerKrispinActive = false;
+    private bool _enemyKrispinActive = false;
 
     //private void Awake()
     //{
@@ -78,6 +85,19 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    void SinkShip(List<GameObject> tileList, int index)
+    {
+        if (_playerTurn)
+        {
+            tileList[index].transform.GetChild(0).gameObject.SetActive(true);
+        }
+        Instantiate(_hitPrefab, tileList[index].transform.position, Quaternion.identity, tileList[index].transform);
+        DecrementShipList(tileList);
+
+        // could pass in a boolean to determine if it's a Krispin action and if yes, 
+        // then play a different ship destruction animation?
+    }
+
     IEnumerator SlowTurnVisualisation(List<GameObject> tileList, int index)
     {
         // Used delay tactics here because otherwise the enemy takes its go so quickly, it looks like it's
@@ -87,25 +107,48 @@ public class GameManager : MonoBehaviour
         {
             yield return new WaitForSeconds(3f);
         }
+
         if (tileList[index].transform.childCount == 0)
         {
             // Instantiate a miss prefab and append it as a child
             Instantiate(_missedPrefab, tileList[index].transform.position, Quaternion.identity, tileList[index].transform);
-        }
-        else
-        {
+        } else {
             // option1 - Animate explosion, get the ship and replace it's image with a burning ship
             // option2 - Another option could be to animate explosion, overlay fire
 
             // Temp solution below so we can visualise enemy hits on player ships
-            if (_playerTurn)
+            if (tileList[index].transform.childCount == 1)
             {
-                tileList[index].transform.GetChild(0).gameObject.SetActive(true);
-            }
-            Instantiate(_hitPrefab, tileList[index].transform.position, Quaternion.identity, tileList[index].transform);
-            DecrementShipList(tileList);
+                if (tileList[index].transform.GetChild(0).gameObject.tag == "Ship")
+                {
+                    SinkShip(tileList, index);
+                }
+                else if(tileList[index].transform.GetChild(0).gameObject.tag == "KrispinPlaceholder")
+                {
+                    ReleaseTheKraken(tileList[index].transform.GetChild(0).gameObject);
+                } else
+                {
+                    // This has to be done separate to the miss action above because if we tried to check the tag of a non exisitne child, the game would crash
+                    Instantiate(_missedPrefab, tileList[index].transform.position, Quaternion.identity, tileList[index].transform);
+                }
+            }   
         }
-        CheckGameOver();
+        ToggleKrakenTurn(tileList);
+        if (!_isKrakenTurn)
+        {
+            CheckGameOver();
+        }
+    }
+
+    void ToggleKrakenTurn(List<GameObject> list)
+    {
+        // This set up is why Krispin sort of gets an extra go at the start 
+        // (technically he gets a go off screen at the moment when initially placed)
+        // If we want to prevent this, this area will need changing or we could remove his go on initial placement
+        if (list == _playerTileList && _playerKrispinActive || list == _enemyTileList && _enemyKrispinActive)
+        {
+            _isKrakenTurn = true;
+        }
     }
 
     void DecrementShipList(List<GameObject> tileList)
@@ -150,7 +193,7 @@ public class GameManager : MonoBehaviour
         } else
         {
             _gameStart = true;
-            PlaceKrispin(_playerTileList);
+            PlaceKrispin(_playerTileList, true);
         }
     }
 
@@ -196,7 +239,6 @@ public class GameManager : MonoBehaviour
             _playerTurn = true;
             StartCoroutine(EnemyBoardRoutine(firstGo));
         }
-
         Debug.Log("Player turn set to: " + _playerTurn);
     }
 
@@ -224,10 +266,10 @@ public class GameManager : MonoBehaviour
         {
             int index = GetUnusedRandomNumber(enemyTilesWithShips);
             GameObject enemyShip = Instantiate(_ship, _enemyTileList[index].transform.position, Quaternion.identity, _enemyTileList[index].transform);
-            //enemyShip.SetActive(false);
+            enemyShip.SetActive(false);  //<--- Comment this out for easy testing but uncomment when done testing
             _enemyShipCount++;
         }
-        PlaceKrispin(_enemyTileList);
+        PlaceKrispin(_enemyTileList, true);
     }
 
     public void ResetGame()
@@ -238,6 +280,8 @@ public class GameManager : MonoBehaviour
         _playerTilesAttacked = new List<int>();
         _playerShipCount = 0;
         _enemyShipCount = 0;
+        _playerKrispinActive = false;
+        _enemyKrispinActive = false;
     }
 
     public bool PlayerHasWon()
@@ -245,28 +289,126 @@ public class GameManager : MonoBehaviour
         return _enemyShipCount == 0;
     }
 
-    void PlaceKrispin(List<GameObject> tileList)
+    void PlaceKrispin(List<GameObject> tileList, bool emptyTilesOnly)
     {
         List<int> numsToAvoid = new List<int>();
         bool krispinPlaced = false;
         while (krispinPlaced == false)
         {
             int num = GetUnusedRandomNumber(numsToAvoid);
-            if (tileList[num].transform.childCount == 0)
+            if (emptyTilesOnly)
             {
+                // Initial placement of trap card Krispin
+                if (tileList[num].transform.childCount == 0)
+                {
+                    GameObject krispinPrefab = Instantiate(_krispinPlaceholder, tileList[num].transform.position, Quaternion.identity, tileList[num].transform);
+                    krispinPrefab.SetActive(false);  //<--- Comment this out for easy testing but uncomment when done testing
+                    krispinPlaced = true;
+                }
+            } else
+            {
+                // Used for placing Krispin on the opposite board
                 GameObject krispinPrefab = Instantiate(_krispin, tileList[num].transform.position, Quaternion.identity, tileList[num].transform);
-                //krispinPrefab.SetActive(false);
+                krispinPrefab.GetComponent<Krispin>().SetKrispinOnTheWarpath(); 
+                SetKrispinReference(tileList, krispinPrefab);
+                SetCorrectKrispinToActive(tileList);
                 krispinPlaced = true;
+                if (tileList[num].transform.childCount == 0)
+                {
+                    return;
+                } else
+                {
+                    KrispinAction(tileList, num);
+                }
             }
         }
-
     }
 
-    // select a random number between 0-36
-    // check the board to see if there is a ship on that tile
-    // if there is repeat the process
-    // if there isn't place Krispin and setActive to false
+    void SetCorrectKrispinToActive(List<GameObject> list)
+    {
+        if (list == _playerTileList)
+        {
+            _playerKrispinActive = true;
+        } else
+        {
+            _enemyKrispinActive = true;
+        }
+    }
 
+    void SetKrispinReference(List<GameObject> tileList, GameObject krispinPrefab)
+    {
+        if (tileList == _playerTileList)
+        {
+            _krispinOnPlayerBoard = krispinPrefab;
+        }
+        else
+        {
+            _krispinOnEnemyBoard = krispinPrefab;
+        }
+    }
 
+    void KrispinAction(List<GameObject> tileList, int index)
+    {
+        if (tileList[index].transform.childCount == 0 || tileList[index].transform.childCount > 2)
+        {
+            // play animation of Krispin looking around or something like that 
+        } else
+        {
+            if (tileList[index].transform.GetChild(0).gameObject.tag == "Ship")
+            {
+                SinkShip(tileList, index);
+                Debug.Log("Krispin sunk a ship");
+            } else
+            {
+                // play animation of Krispin looking around or something like that
+            }
+        }
+    }
 
+    public IEnumerator MoveKrispin(GameObject krispin, List<int> visitedTiles, string boardName)
+    {
+        yield return new WaitForSeconds(1.5f);
+        
+            int num = GetUnusedRandomNumber(visitedTiles);
+            if (boardName == "PlayerBoard")
+            {
+                krispin.transform.SetParent(_playerTileList[num].transform, false);
+                KrispinAction(_playerTileList, num);
+            } else
+            {
+                krispin.transform.SetParent(_enemyTileList[num].transform, false);
+                KrispinAction(_enemyTileList, num);
+            }
+    
+        CheckGameOver();
+    }
+
+    void ReleaseTheKraken(GameObject krispinMarker)
+    {        
+        PlaceKrispinOnCorrectBoard();
+        GameObject tile = krispinMarker.transform.parent.gameObject;
+        Instantiate(_skull, tile.transform.position, Quaternion.identity, tile.transform);
+        Destroy(krispinMarker);
+    }
+
+    void PlaceKrispinOnCorrectBoard()
+    {
+        if (_playerTurn)
+        {
+            PlaceKrispin(_playerTileList, false);
+        } else
+        {
+            PlaceKrispin(_enemyTileList, false);
+        }
+    }
+
+    public bool IsKrispinsTurn()
+    {
+        return _isKrakenTurn;
+    }
+
+    public void SetKrakenTurn(bool val)
+    {
+        _isKrakenTurn = val;
+    }
 }
